a41b98e7b8478bccee5922606a7cbcbda1538321
Jan Lewandowski <lewandowski.jan93@gmail.com>
2022-12-28 19:14:48 +0100

    fix test

--- src/app/pages/all-prices-page/all-prices-page/all-prices-page.component.spec.ts
+++ src/app/pages/all-prices-page/all-prices-page/all-prices-page.component.spec.ts
@@ -1,23 +1,32 @@
+import { DataService } from 'src/app/services/data.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AllPricesPageComponent } from './all-prices-page.component';
+import { of } from 'rxjs';
 
 describe('AllPricesPageComponent', () => {
-  let component: AllPricesPageComponent;
-  let fixture: ComponentFixture<AllPricesPageComponent>;
+    let component: AllPricesPageComponent;
+    let fixture: ComponentFixture<AllPricesPageComponent>;
+    const mockedDataService = jasmine.createSpyObj('DataService', ['getAllPrices']);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AllPricesPageComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: DataService,
+                    useValue: mockedDataService,
+                },
+            ],
+            declarations: [AllPricesPageComponent],
+        }).compileComponents();
+        fixture = TestBed.createComponent(AllPricesPageComponent);
+        component = fixture.componentInstance;
 
-    fixture = TestBed.createComponent(AllPricesPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        mockedDataService.getAllPrices.and.returnValue(of([]));
+        expect(component).toBeTruthy();
+    });
 });
